d59837068fb6494d4fd62350000fd9f34a7f0622
prabin <kabu.s.prabin@gmail.com>
2024-01-29 21:09:33 +0530

    firebase add

--- components/Registration.js
+++ components/Registration.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { FlatList, Image, Text, View, Button, TextInput, } from 'react-native';
 import SelectDropdown from 'react-native-select-dropdown';
+import database from '@react-native-firebase/database';
 
+const reference = database().ref('/user/emp');
+const [userdata , setuserdata] = useState(null);
 const regions  = ["Odisha", "ER_1", "ER_2", "CC"]
 const substation  = ["Pandiabili", "Kaniha", "Indravati", "Rhq_bbsr"]
 const Registration = () => {
+  useEffect(()=>{
+    getDatabase();
+  },[]);
+ const getDatabase = async ()=>{
+  try{
+    const data = await database().ref("user/emp").once('value');
+    console.log(data)
+    setuserdata(data.val())
+  }
+  catch(err){
+    console.log(err);
+  }
+ }
   return (
+   
     <View style={{ flex: 1, alignItems: 'center' }}>
       <Text>Registration</Text>
       <TextInput placeholder='Name' />
